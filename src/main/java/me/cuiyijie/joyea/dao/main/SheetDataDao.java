f0924ebfd8e8a4e18963c223733e6904d3f4f760
yjcui3 <yjcui3@iflytek.com>
2021-12-23 17:27:43 +0800

    feat:增加功能

--- src/main/java/me/cuiyijie/joyea/dao/main/SheetDataDao.java
+++ src/main/java/me/cuiyijie/joyea/dao/main/SheetDataDao.java
@@ -1,6 +1,10 @@
 package me.cuiyijie.joyea.dao.main;
 
+import me.cuiyijie.joyea.model.SheetData;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @Author: cuiyijie
@@ -10,5 +14,12 @@ import org.springframework.stereotype.Repository;
 public interface SheetDataDao {
 
 
+    Integer insert(@Param("item") SheetData sheetData);
+
+    Integer update(@Param("item") SheetData sheetData);
+
+    Integer delete(@Param("item") SheetData sheetData);
+
+    List<SheetDao> list(@Param("item") SheetData sheetData);
 
 }
