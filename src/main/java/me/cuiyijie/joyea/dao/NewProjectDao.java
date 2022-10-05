ca2ab24892de54971ee074fa4f4ad9d242b2e3bd
yjcui3 <yjcui3@iflytek.com>
2022-10-05 15:25:41 +0800

    feat:新建第三版本分支

---
+++ src/main/java/me/cuiyijie/joyea/dao/NewProjectDao.java
@@ -0,0 +1,13 @@
+package me.cuiyijie.joyea.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import me.cuiyijie.joyea.model.NewProject;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author: yjcui3
+ * @Date: 2022/9/28 11:28
+ */
+@Repository
+public interface NewProjectDao extends BaseMapper<NewProject> {
+}
--- src/test/java/me/cuiyijie/joyea/JoyeaProjectManagerApplicationTests.java
+++ src/test/java/me/cuiyijie/joyea/JoyeaProjectManagerApplicationTests.java
@@ -1,14 +1,26 @@
 package me.cuiyijie.joyea;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import me.cuiyijie.joyea.dao.NewProjectDao;
+import me.cuiyijie.joyea.model.NewProject;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class JoyeaProjectManagerApplicationTests {
 
 
+    @Autowired
+    private NewProjectDao newProjectDao;
+
+
     @Test
     void contextLoads() {
+        List<NewProject> projectList = newProjectDao.selectList(new QueryWrapper<>());
+        System.out.println(projectList);
     }
 
 }
