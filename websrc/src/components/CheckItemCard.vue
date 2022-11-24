efa31d47314ac5945ee31f9cf628e7b9b23fdf88
cuiyijie <cyj976655@gmail.com>
2022-11-24 20:25:06 +0800

    fix:修复问题

--- websrc/src/components/CheckItemCard.vue
+++ websrc/src/components/CheckItemCard.vue
@@ -38,7 +38,7 @@
         <van-col span="24" v-if="item.cfCheckResult !== null">
           <van-image v-if="item.cfCheckResult === '1'" :src="qualifiedImg" width="100" height="100"/>
           <van-image v-if="item.cfCheckResult === '2'" :src="unQualifiedImg" width="100" height="100"/>
-          <van-image v-if="item.cfCheckResult === '3'" :src="unknownImg" width="100" height="100"/>
+          <van-image v-if="item.cfCheckResult === '3'" :src="unKnownImg" width="100" height="100"/>
         </van-col>
       </van-row>
     </div>
@@ -62,7 +62,7 @@ export default {
       defaultImg: require("@/assets/unknown.png"),
       qualifiedImg: require("@/assets/qualified.png"),
       unQualifiedImg: require("@/assets/unqualified.png"),
-      unknownImg: require("@/assets/unknown.png"),
+      unKnownImg: require("@/assets/unknown.png"),
       isOpen: false,
     }
   },
